37815c7bf08903174bf25b4bf8277d68ea4097c9
Ubuntu <ubuntu@ip-172-31-82-156.ec2.internal>
2025-06-01 07:49:34 +0000

    This is my version 3

---
+++ vic.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#
+echo "Welcome to the world of devops:::...!!"
+
+echo "I hate reading."
+
+# This is just for practising
+#
+echo "This is the field of devops"
+
+# we are welcoming you to the world of devops.
+
+
+echo "Testing is a great field, if you know the automation."
